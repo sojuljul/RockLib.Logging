7cb7232e7290f93095ae3759388cf769e711f4df
Juliano Nguyen <julianonguyen@quickenloans.com>
2021-06-25 15:52:50 -0400

    add json formatter

---
+++ RockLib.Logging/LogProviders/JsonMessageFormatter.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace RockLib.Logging.LogProviders
+{
+    /// <summary>
+    /// An implementation of the <see cref="ILogFormatter"/> interface that uses a template
+    /// to format a log entry into JSON for Kubernetes.
+    /// </summary>
+    public class JsonMessageFormatter : ILogFormatter
+    {
+        /// <summary>
+        /// Formats the specified log entry to a JSON.
+        /// </summary>
+        /// <param name="logEntry">The log entry to format.</param>
+        /// <returns>The formatted log entry.</returns>
+        public string Format(LogEntry logEntry)
+        {
+            var o = new
+            {
+                Level = GetLogLevelName(logEntry.Level),
+                Time = logEntry.CreateTime.ToString("o"),
+                Body = logEntry.Message,
+            };
+            return JsonConvert.SerializeObject(o, new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() });
+        }
+
+        private string GetLogLevelName(LogLevel logLevel)
+        {
+            switch (logLevel)
+            {
+                case LogLevel.Audit:
+                    return nameof(LogLevel.Audit);
+                case LogLevel.Debug:
+                    return nameof(LogLevel.Debug);
+                case LogLevel.Error:
+                    return nameof(LogLevel.Error);
+                case LogLevel.Fatal:
+                    return nameof(LogLevel.Fatal);
+                case LogLevel.Info:
+                    return nameof(LogLevel.Info);
+                case LogLevel.NotSet:
+                    return nameof(LogLevel.NotSet);
+                case LogLevel.Warn:
+                    return nameof(LogLevel.Warn);
+                default:
+                    return "Unknown";
+            }
+        }
+    }
+}
---
+++ Tests/RockLib.Logging.Tests/LogProviderTests/JsonMessageFormatterTests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RockLib.Logging.LogProviders;
+using Xunit;
+
+namespace RockLib.Logging.Tests.LogProviderTests
+{
+    public class JsonMessageFormatterTests
+    {
+        [Fact]
+        public void JsonMessageFormatter_LogLevelDebug()
+        {
+            var jsonFormatter = new JsonMessageFormatter();
+            var logEntry = new LogEntry("testMessage", LogLevel.Debug);
+            var test = jsonFormatter.Format(logEntry);
+            Assert.Contains("Debug", test);
+        }
+
+        [Fact]
+        public void JsonMessageFormatter_LogLevelAudit()
+        {
+            var jsonFormatter = new JsonMessageFormatter();
+            var logEntry = new LogEntry("testMessage", LogLevel.Audit);
+            var test = jsonFormatter.Format(logEntry);
+            Assert.Contains("Audit", test);
+        }
+
+        [Fact]
+        public void JsonMessageFormatter_LogLevelError()
+        {
+            var jsonFormatter = new JsonMessageFormatter();
+            var logEntry = new LogEntry("testMessage", LogLevel.Error);
+            var test = jsonFormatter.Format(logEntry);
+            Assert.Contains("Error", test);
+        }
+
+        [Fact]
+        public void JsonMessageFormatter_LogLevelFatal()
+        {
+            var jsonFormatter = new JsonMessageFormatter();
+            var logEntry = new LogEntry("testMessage", LogLevel.Fatal);
+            var test = jsonFormatter.Format(logEntry);
+            Assert.Contains("Fatal", test);
+        }
+
+        [Fact]
+        public void JsonMessageFormatter_LogLevelInfo()
+        {
+            var jsonFormatter = new JsonMessageFormatter();
+            var logEntry = new LogEntry("testMessage", LogLevel.Info);
+            var test = jsonFormatter.Format(logEntry);
+            Assert.Contains("Info", test);
+        }
+
+        [Fact]
+        public void JsonMessageFormatter_LogLevelWarn()
+        {
+            var jsonFormatter = new JsonMessageFormatter();
+            var logEntry = new LogEntry("testMessage", LogLevel.Warn);
+            var test = jsonFormatter.Format(logEntry);
+            Assert.Contains("Warn", test);
+        }
+    }
+}
